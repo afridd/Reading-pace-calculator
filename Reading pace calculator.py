ff4f786a9e1d9bf8393b7a336068f2eff4870e28
Afrid <afridashrafali@gmail.com>
2026-07-19 21:10:15 +0530

    Reduced the size of the program and made it more efficient and faster with same functionality

--- Reading pace calculator.py
+++ Reading pace calculator.py
@@ -1,4 +1,4 @@
-pages = int(input("Enter the number of pages: "))
+pages= int(input("Enter the number of pages: "))
 days = int(input("Enter the number of days: "))
 
 if pages <= 0 or days <= 0:
@@ -6,18 +6,23 @@ if pages <= 0 or days <= 0:
 else:
   calculation = pages/days
   if calculation.is_integer():
-      pages_per_day = int(calculation)
+    pages_per_day = int(calculation)
   else:
-     pages_per_day= round(calculation,2)
+    pages_per_day= round(calculation,2)
+  if pages_per_day==1:
+    page_str="page"
+  else:
+    page_str="pages"
+  if days==1:
+    days_str="day"
+  else:
+    days_str="days"
+  print(f"You must read {pages_per_day} {page_str} to complete the book in {days} {days_str}")
+      
+      
+    
   
  
-  if pages_per_day == 1 and days == 1:
-    print(f"You must read {pages_per_day} page to complete the book in {days} day")
-  elif pages_per_day==1 and days>1:
-    print(f"You must read {pages_per_day} page to complete the book in {days} days")
-  elif pages_per_day>1 and days==1:
-    print(f"You must read {pages_per_day} pages to complete the book in {days} day")
-  elif pages_per_day>1 and days>1:
-    print(f"You must read {pages_per_day} pages to complete the book in {days} days")
-  else:
-    print(f"You must read {pages_per_day} pages to complete the book in {days} days")
+  
+    
+ 
