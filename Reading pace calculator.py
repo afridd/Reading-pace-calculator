c1136f530ae3a8f380248bffd7b87d6a7c42c886
Afrid <afridashrafali@gmail.com>
2026-07-20 11:59:51 +0530

    Debugging the syntax errors

--- Reading pace calculator.py
+++ Reading pace calculator.py
@@ -1,17 +1,15 @@
-pages= int(input("Enter the number of pages: "))
+pages = int(input("Enter the number of pages: "))
 days = int(input("Enter the number of days: "))
 
 if pages <= 0 or days <= 0:
-  print("You must enter value greater than 0")
+    print("You must enter value greater than 0")
 else:
-  calculation = pages/days
-  
-  # Consolidate integer check into a single line
-  pages_per_day = int(calculation) if calculation.is_integer() else pages_per_day= round(calculation,2)
+    calculation = pages / days
     
-  # Use inline conditional logic inside the f-string to handle pluralization  
-  page_str="page" if pages_per_day==1 else page_str="pages"
-  days_str="day" if days==1 else days_str="days"  
+    pages_per_day = int(calculation) if calculation.is_integer() else round(calculation, 2)
   
-  print(f"\nYou must read {pages_per_day} {page_str} to complete the book in {days} {days_str}")
+    page_str = "page" if pages_per_day == 1 else "pages"
+    days_str = "day" if days == 1 else "days"  
+    
+    print(f"\nYou must read {pages_per_day} {page_str} to complete the book in {days} {days_str}")
   
